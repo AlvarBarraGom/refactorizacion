2b47cb8acf09a59be20a54cd9aed4a38f89b68ab
AlvarBarraGom <algomezbarra@gmail.com>
2019-04-01 12:30:40 +0200

    ConIva

--- EntornosTema6/src/es/studium/Refactorizacion/EjercicioPrecio.java
+++ EntornosTema6/src/es/studium/Refactorizacion/EjercicioPrecio.java
@@ -3,6 +3,7 @@ package es.studium.Refactorizacion;
 public class EjercicioPrecio
 {
 	public double calcularPrecio (int unidades, double precioUnitario) {
-		return (unidades*precioUnitario);
+		
+		return (unidades*precioUnitario)/1.21;
 	}
 }
