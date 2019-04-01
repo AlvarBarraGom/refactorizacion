9b728552b14058d212b8ddc4953b9deeb8efb030
AlvarBarraGom <algomezbarra@gmail.com>
2019-04-01 12:57:15 +0200

    Constante

--- EntornosTema6/src/es/studium/Refactorizacion/EjercicioPrecio.java
+++ EntornosTema6/src/es/studium/Refactorizacion/EjercicioPrecio.java
@@ -2,8 +2,12 @@ package es.studium.Refactorizacion;
 
 public class EjercicioPrecio
 {
+	final int IVA = 21;
+	
 	public double calcularPrecio (int unidades, double precioUnitario) {
 		
-		return (unidades*precioUnitario)*1.21;
+		double precioSinIva = unidades*precioUnitario;
+		double precioConIva = precioSinIva*IVA;
+		return precioConIva;
 	}
 }
